ad202b4f49cc438f99287ebfbcfcae0d54ec19d8
Vatsalya-singhi <vatsalya.singhi4316@gmail.com>
2022-11-19 01:51:17 +0530

    material component

--- src/app/pages/component2/component2.component.ts
+++ src/app/pages/component2/component2.component.ts
@@ -1,9 +1,13 @@
+import { PagesService } from './../pages.service';
 import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Location } from '@angular/common';
-import { PagesService } from '../pages.service';
+
+import { CollectionViewer, DataSource } from "@angular/cdk/collections";
+import { BehaviorSubject, catchError, finalize, Observable, of } from 'rxjs';
+import { addDoc, collection, collectionData, CollectionReference, doc, DocumentData, Firestore, setDoc } from '@angular/fire/firestore';
 
 export interface UserData {
     id: string;
@@ -44,34 +48,39 @@ const NAMES: string[] = [
     'Thomas',
     'Elizabeth',
 ];
+
 @Component({
     selector: 'app-component2',
     templateUrl: './component2.component.html',
     styleUrls: ['./component2.component.scss']
 })
 export class Component2Component implements OnInit {
-    displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
-    dataSource: MatTableDataSource<UserData>;
+    public displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+    public dataSource: MatTableDataSource<UserData>;
+    public actualValue: number = 0;
+    public userValue: number = 0;
+
+    // dataSourceMock = new UserDataSource(this.pageService);
 
     @ViewChild(MatPaginator)
-    paginator!: MatPaginator;
+    public paginator!: MatPaginator;
     @ViewChild(MatSort)
-    sort!: MatSort;
+    public sort!: MatSort;
 
-    constructor(public pageService: PagesService, public _location: Location) {
-        // Create 100 users
+
+    constructor(
+        public pageService: PagesService,
+        public _location: Location,
+        public firestore: Firestore
+    ) {
         const users = Array.from({ length: 100 }, (_, k) => this.createNewUser(k + 1));
-        // Assign the data to the data source for the table to render
+        this.actualValue = users.filter((val) => val.fruit == "kiwi").length ?? 0;
+
         this.dataSource = new MatTableDataSource(users);
     }
 
     public ngOnInit(): void {
-        this.pageService.getAllLaunches()
-            .subscribe((data) => {
-                console.log("launch list=>", data);
-            }, (err) => {
-                console.log(err);
-            })
+
     }
 
     public ngAfterViewInit() {
@@ -105,9 +114,50 @@ export class Component2Component implements OnInit {
 
     }
 
-    public backClicked() {
-        this._location.back();
+
+
+
+    // public logList: any[] = [];
+    // public getLogs() {
+    //     const col: CollectionReference<DocumentData> = collection(this.firestore, 'spaceXlog');
+    //     collectionData(col).subscribe((data) => {
+    //         this.logList = data;
+    //         console.log("this.logList =>", this.logList);
+    //     })
+    // }
+
+}
+
+
+export class UserDataSource extends DataSource<any> {
+    private lessonsSubject = new BehaviorSubject<any[]>([]);
+    private loadingSubject = new BehaviorSubject<boolean>(false);
+    public loading$ = this.loadingSubject.asObservable();
+
+    constructor(private pageService: PagesService) {
+        super();
     }
 
+    connect(collectionViewer: CollectionViewer): Observable<any[]> {
+        return this.lessonsSubject.asObservable();
+    }
+
+    disconnect(collectionViewer: CollectionViewer): void {
+        this.lessonsSubject.complete();
+        this.loadingSubject.complete();
+    }
+
+    loadLessons(courseId: number, filter = '',
+        sortDirection = 'asc', pageIndex = 0, pageSize = 3) {
+
+        this.loadingSubject.next(true);
+
+        this.pageService.findLessons(courseId, filter, sortDirection, pageIndex, pageSize)
+            .pipe(
+                catchError(() => of([])),
+                finalize(() => this.loadingSubject.next(false))
+            )
+            .subscribe((lessons: any) => this.lessonsSubject.next(lessons));
+    }
 
 }
--- src/app/pages/pages.service.ts
+++ src/app/pages/pages.service.ts
@@ -1,10 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
     providedIn: 'root'
 })
+
 export class PagesService {
 
     constructor(
@@ -13,6 +15,19 @@ export class PagesService {
 
     public getAllLaunches(): Observable<Object> {
         return this.http.get(`https://api.spacexdata.com/v5/launches`);
+    }
+
+    public findLessons(courseId: number, filter = '', sortOrder = 'asc', pageNumber = 0, pageSize = 3): Observable<any[]> {
+        return this.http.get('/api/lessons', {
+            params: new HttpParams()
+                .set('courseId', courseId.toString())
+                .set('filter', filter)
+                .set('sortOrder', sortOrder)
+                .set('pageNumber', pageNumber.toString())
+                .set('pageSize', pageSize.toString())
+        }).pipe(
+            map((res: any) => res.payload)
+        );
     }
 
 }
